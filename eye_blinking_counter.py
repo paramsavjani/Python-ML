6856bb2dfa96a543c8ed42a566d32602561897aa
Param savjani <paramsavjani3010@gmail.com>
2024-06-24 05:36:40 +0530

    f

--- eye_blinking_counter.py
+++ eye_blinking_counter.py
@@ -1,10 +1,26 @@
 from asyncio import sleep
 import cvzone.FaceMeshModule as fm
 import cv2
+import cvzone
+from cvzone.PlotModule import LivePlot
 
-cap = cv2.VideoCapture("./video.mp4")
+
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture("./video.mp4")
 detector = fm.FaceMeshDetector(maxFaces=2)
-idList = [7, 33, 157, 158, 159, 160, 161, 163, 144, 145, 155, 153, 154, 173, 133]
+plotY = LivePlot(640, 480, [20, 50], invert=True, char=" ")
+
+
+newidList = [7, 33, 157, 158, 159, 160, 161, 163, 144, 145, 155, 153, 154, 173, 133]
+
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243, 173, 133]
+
+
+ratioList = []
+blinkCounter = 0
+counter = 0
+color = (255, 0, 255)
+
 
 while True:
 
@@ -12,21 +28,48 @@ while True:
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     success, img = cap.read()
-    img, faces = detector.findFaceMesh(img)
-    if faces[0]:
+    img, faces = detector.findFaceMesh(img, draw=False)
+    if faces:
         face = faces[0]
-        for id in idList:
-            cv2.circle(img, face[id], 6, (0, 255, 0), cv2.FILLED)
+
+        # for id in newidList:
+        #     cv2.circle(img, face[id], 6, (0, 255, 0), cv2.FILLED)
 
         leftUp = face[159]
-        leftDown = face[145]
+        leftDown = face[23]
         leftLeft = face[130]
         leftRight = face[243]
 
         lenghtVer, _ = detector.findDistance(leftUp, leftDown)
         lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
-        
-        
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(100000)
+        ratio = int((lenghtVer / lenghtHor) * 100)
+
+        ratioList.append(ratio)
+
+        if len(ratioList) > 2:
+            ratioList.pop(0)
+
+        ratioAvg = sum(ratioList) / len(ratioList)
+
+        if ratioAvg < 35 and counter == 0:
+            blinkCounter += 1
+            color = (0, 200, 0)
+            counter = 1
+        if counter != 0:
+            counter += 1
+            if counter > 10:
+                counter = 0
+                color = (255, 0, 255)
+
+        cvzone.putTextRect(img, f"Blink Count: {blinkCounter}", (50, 100), colorR=color)
+
+        imgPlot = plotY.update(ratio, color)
+        img = cv2.resize(img, (640, 400))
+        imgStack = cvzone.stackImages([img, imgPlot], 1, 1)
+    else:
+        img = cv2.resize(img, (640, 400))
+        imgStack = cvzone.stackImages([img, img], 2, 1)
+
+    cv2.imshow("Image", imgStack)
+    cv2.waitKey(25)
