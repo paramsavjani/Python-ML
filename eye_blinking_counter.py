3b9c532f246666704dc5eb69ef32de6c40bd3be4
Param savjani <paramsavjani3010@gmail.com>
2024-06-24 19:55:29 +0530

    f

--- eye_blinking_counter.py
+++ eye_blinking_counter.py
@@ -4,25 +4,25 @@ from cvzone.FaceMeshModule import FaceMeshDetector
 from cvzone.PlotModule import LivePlot
 from collections import deque
 
-# Initialize camera
-cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture("./video.mp4")
 
-# Initialize face mesh detector
+cap = cv2.VideoCapture(0)
+
+
+
 detector = FaceMeshDetector(maxFaces=1)
 
-# Initialize live plot for eye aspect ratio
+
 plotY = LivePlot(640, 480, [20, 50], invert=True, char=" ")
 
-# Define landmarks for eye aspect ratio calculation
+
 idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243, 173, 133]
 
-# Initialize variables
+
 ratioList = deque(maxlen=2)
 blinkCounter = 0
 color = (255, 0, 255)
 flag = False
-blinkThreshold = 35  # Eye aspect ratio threshold to detect blink
+blinkThreshold = 35  
 
 
 def calculate_eye_aspect_ratio(face):
@@ -38,24 +38,25 @@ def calculate_eye_aspect_ratio(face):
 
 
 while True:
-    # Restart video if it reaches the end
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    
+    # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+    #     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     success, img = cap.read()
     img = cv2.flip(img, 1)
 
-    # Detect face mesh
+    
     img, faces = detector.findFaceMesh(img, draw=False)
+    
     if faces:
         face = faces[0]
 
-        # Calculate eye aspect ratio
+        
         ratio = calculate_eye_aspect_ratio(face)
         ratioList.append(ratio)
         ratioAvg = sum(ratioList) / len(ratioList)
 
-        # Blink detection logic
+        
         if ratioAvg < blinkThreshold and not flag:
             flag = True
             color = (0, 200, 0)
@@ -64,10 +65,10 @@ while True:
             blinkCounter += 1
             color = (200, 0, 0)
 
-        # Display blink count
+        
         cvzone.putTextRect(img, f"Blink Count: {blinkCounter}", (40, 100), colorR=color)
 
-        # Update plot
+        
         imgPlot = plotY.update(ratio, color)
         img = cv2.resize(img, (640, 400))
         imgStack = cvzone.stackImages([img, imgPlot], 1, 1)
@@ -75,7 +76,7 @@ while True:
         img = cv2.resize(img, (640, 400))
         imgStack = cvzone.stackImages([img, img], 2, 1)
 
-    # Show image
+    
     cv2.imshow("Image", imgStack)
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
